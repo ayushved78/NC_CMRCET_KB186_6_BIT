e19d577abb46106ebf7b472aa9e8bec11d9dc961
Abhinand01 <abhinandpreman@pec.edu>
2020-08-01 22:55:03 +0530

    test 3

--- lib/Tabs/developers.dart
+++ lib/Tabs/developers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class developers extends StatefulWidget {
   @override
@@ -8,12 +9,151 @@ class developers extends StatefulWidget {
 class _developersState extends State<developers> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Icon(
-        Icons.developer_board,
-        size: 85.0,
-        color: Colors.white,
+    return SingleChildScrollView(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            color: Color(0xFF),
+            child: Column(
+              children: <Widget>[
+                Divider(
+                  thickness: 10.0,
+                  color: Colors.black12,
+                  height: 25.0,
+                ),
+                Text(
+                  "~~WELCOME~~",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Divider(
+                  thickness: 10.0,
+                  color: Colors.black12,
+                  height: 25.0,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+
+                //poster
+
+                Container(
+                  child: Image(
+                    image: AssetImage('images/banner.png'),
+                    width: double.infinity,
+                  ),
+                ),
+                Divider(
+                  thickness: 10.0,
+                  color: Colors.black12,
+                  height: 25.0,
+                ),
+
+                //about app
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      Center(
+                        child: Text(
+                          "About the app",
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.blue.shade900,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0,),
+                      Text(
+                        "This application is developed by team 6_BIT of Pondicherry Engineering College, Puducherry for SIH-2020. This application provides information about cyber team of different States/UT. Here in this app we have stored the information like phone number and email ID of cyber police as per states and UT. In addition we have provided the WhatsApp API of Delhi police team where you can forward the disputed messages!!",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ),
+                SizedBox(height: 15.0,),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Divider(
+                        thickness: 10.0,
+                        color: Colors.black12,
+                        height: 25.0,
+                      ),
+                      Center(
+                        child: Text(
+                          "CREDITS",
+                          style: TextStyle(
+                            fontSize: 35.0,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0,),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'AYUSH SINGH',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 22.5,
+                            ),
+                          ),
+                          Text(
+                            'ABHINAND PREMAM',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 22.5,
+                            ),
+                          ),
+                          Text(
+                            'RAJKUMARI KIRAN',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 22.5,
+                            ),
+                          ),
+                          Text(
+                            'MANISH KUMAR',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 22.5,
+                            ),
+                          ),
+                          Text(
+                            'QAZI FARMAAN ARIF',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 22.5,
+                            ),
+                          ),
+                          Text(
+                            'G. PREETHAM SAI',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 22.5,
+                            ),
+                          ),
+
+
+
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
