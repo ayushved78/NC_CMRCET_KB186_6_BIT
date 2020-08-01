14414d50256575600f59d71c1170bfc415edb660
Abhinand01 <abhinandpreman@pec.edu>
2020-08-01 12:15:43 +0530

    test 1

--- lib/states/statedetail.dart
+++ lib/states/statedetail.dart
@@ -5,17 +5,11 @@ class stateDetails extends StatefulWidget {
   final state_name_detail;
   final state_picture;
   final state_stm;
-  final state_phn;
-  final state_em;
-  final state_wh;
   stateDetails(
   {
     this.state_name_detail,
     this.state_picture,
     this.state_stm,
-    this.state_phn,
-    this.state_em,
-    this.state_wh,
 }
       );
 
@@ -24,6 +18,159 @@ class stateDetails extends StatefulWidget {
 }
 
 class _stateDetailsState extends State<stateDetails> {
+  var state_det = [
+    {
+      "phone": "0863-2340152" ,
+      "email": "cybercrimescid@ap.gov.in" ,
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0360-2215518",
+      "email": "spcr@arunpol.nic.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0361-2524494" ,
+      "email": "digpcid@assampolice.gov.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0612-2238098",
+      "email": "cybercellbih@nic.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0771-2511623",
+      "email": "aigtechphq.cg@gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "011-20892633",
+      "email": "acp.cybercell@delhipolice.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0832-2420883",
+      "email": "picyber@goapolice.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "079-23250798",
+      "email": "cccid@gujarat.gov.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "01733-253230" ,
+      "email": "shocyberpkl.polhry@gov.in" ,
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0177-2627955",
+      "email": "polcyberps-shihp@nic.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0651-2490046",
+      "email": "cyberps@jhpolice.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0191-2582996",
+      "email": "igcrimejk@nic.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "9480801554",
+      "email": "digadmincod@ksp.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0471-2722215",
+      "email": "igpcrimes.pol@kerala.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0755-2770248",
+      "email": "mpcyberpolice@mppolice.gov.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "022-22160080",
+      "email": "sp.cbrmah@gov.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0385-2450573",
+      "email": "cidcbmn@nic.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0364-2550141",
+      "email": "sspcidmeg@gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0389-2334682",
+      "email": "cidcrimemz@nic.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0370-2223897",
+      "email": "cybercrimepsngl@gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0671-2306071",
+      "email": "dirscrb.odpol@nic.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0172-2226258",
+      "email": "aigcc@punjabpolice.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0141-2309547",
+      "email": "ccps-raj@nic.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "03592-204297",
+      "email": "spcid@sikkimpolice.nic.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "044-28447712",
+      "email": "sp1ccdtnpolice@gov.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "040-23147604",
+      "email": "ccps.cid@tspolice.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0381-2321741",
+      "email": "spcybercrime@tripurapolice.nic.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0135-2712563",
+      "email": "ccps.deh@uttarakhandpolice.uk.gov.in ",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "0522-2208598",
+      "email": "spcyber.lu@up.gov.in",
+      "whno": "9087200100",
+    },
+    {
+      "phone": "033-24791830",
+      "email": "ccpwb@cidwestbengal.gov.in",
+      "whno": "9087200100",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
