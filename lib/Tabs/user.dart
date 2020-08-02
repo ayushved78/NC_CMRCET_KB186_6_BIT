55610337d00a56f973d6c22ce887e77dcc9c7fc6
manishkumardubb <62064588+manishkumardubb@users.noreply.github.com>
2020-08-02 14:24:22 +0530

    Update user.dart

--- lib/Tabs/user.dart
+++ lib/Tabs/user.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class User extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return  Container(
       color: Colors.red,
       child: Icon(
         Icons.supervised_user_circle,
